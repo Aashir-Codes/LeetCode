1691f4f0e84b0b6d0f5d7bdbf465d2735c97fb6a
Aashir-Codes <khanaashir038@gmail.com>
2026-06-29 17:18:05 +0500

    Time: 3 ms (14.72%), Space: 23.8 MB (82.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,27 +4,18 @@ public:
         
         // To solve this  problem i have two sols one naive in which i take another array take zero and then add non zero numbers 
         //other use insertion sort which is known for stable sorting 
-    int size = nums.size();
-    cout<<size;
-
-     vector<int> arr{};
+         int size = nums.size();
+         int pos=0;
 
 
-     for (int i=0;i<size;i++)
-     {
-        if(nums[i] != 0)
-        arr.push_back(nums[i]);
-     }
+         for (int i=0;i<size;i++)
+             if(nums[i]!=0)
+             nums[pos++]=nums[i];
 
-     
+         for (int i=pos;i<size;i++)
+              nums[i]=0;    
 
-     for (int i=0;i<size;i++)
-     {
-        if(nums[i] == 0)
-        arr.push_back(nums[i]);
-     }
-
-nums = arr;
+    
     }
     
 };
