ba665c8bb9f7e7ee2289bed3715f038f71aeb3af
Aashir <khanaashir038@gmail.com>
2026-09-05 11:02:17 +0500

    Time: 0 ms (100%), Space: 8.6 MB (71.69%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,21 @@
-#include <cmath>
-
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+    double myPow(double x, long n) {
+    if (n == 0) return 1.0;
+
+    
+    if (n < 0) {
+        x = 1.0 / x;
+        n = -(n);
     }
+
+    
+    if (n % 2 == 0) {
+        return myPow(x * x, n / 2);
+    }
+    
+    else {
+        return x * myPow(x * x, n / 2);
+    }
+}
 };
