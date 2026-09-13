d6c03ecd56e292e003dd167cba94aa9072c960ca
Aashir <khanaashir038@gmail.com>
2026-09-13 16:31:29 +0500

    Time: 0 ms (100%), Space: 20.7 MB (24.22%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int number=0;
+        for (auto num : nums)
+        {
+            number^=num;
+
+        }
+        return number;
+    }
+};
